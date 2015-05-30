1e7c0fee7d1c2e6edd08bc650ce382b6f6f19f90
Rod Schmidt <rod@infinitenil.com>
2015-05-30 16:34:03 -0600

    Add distance and servo to startup status

--- lib/exgopigo/brain.ex
+++ lib/exgopigo/brain.ex
@@ -1,6 +1,9 @@
 defmodule ExGoPiGo.Brain do
 	alias ExGoPiGo.Board
 	alias ExGoPiGo.LEDs
+	alias ExGoPiGo.Servo
+	alias ExGoPiGo.UltraSonicSensor
+	require Logger
 
 	@moduledoc """
 	The brain for our robot. Controls and responds to all the sensors, motors, etc.
@@ -26,6 +29,25 @@ defmodule ExGoPiGo.Brain do
 		LEDs.turnOnRightLED(pid)
 		:timer.sleep(1000)
 		LEDs.turnOffRightLED(pid)
+
+		# Servo.turn_to_degrees(pid, 0)
+		# :timer.sleep(1000)
+		# Servo.turn_to_degrees(pid, 180)
+		# :timer.sleep(1000)
+		# Servo.turn_to_degrees(pid, 90)
+		# :timer.sleep(500)
+
+		test_servo(pid, 0)
+
+		Logger.info "Distance is #{UltraSonicSensor.distance(pid)} cm."
+	end
+
+	def test_servo(pid, degrees) do
+		Servo.turn_to_degrees(pid, degrees)
+		:timer.sleep(250)
+		if degrees + 30 <= 180 do
+			test_servo(pid, degrees + 30)
+		end
 	end
 
 end
